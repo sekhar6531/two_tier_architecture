2cd6c60151bd1673e8009fc86c0f4a8fda307094
sekhar6531 <kulasekhar6531@gmail.com>
2023-11-17 19:08:18 +0530

    first commit

---
+++ nz/urls.py
@@ -0,0 +1,6 @@
+from nz.views import *
+from django.urls import path
+app_name='boult'
+urlpatterns=[
+    path('kane/',kane,name='kane'),
+]
---
+++ nz/views.py
@@ -0,0 +1,6 @@
+from django.shortcuts import render
+
+# Create your views here.
+from django.http import HttpResponse
+def kane(request):
+    return HttpResponse('Best captain in NZ ')
